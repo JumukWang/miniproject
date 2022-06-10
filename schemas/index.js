2041e93b500612166472e3e3aad755a545f87f21
hayeonkimm <hayeon.data@gmail.com>
2022-06-10 21:10:48 +0900

    .env 추가

--- schemas/index.js
+++ schemas/index.js
@@ -1,16 +1,32 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const DB = mongoose.connection;;
+
+
+// const connect = () => {
+//   mongoose.connect('mongodb://localhost:27017/restaurant', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+// };
+
+// mongoose.connection.on("error", (err) => {
+//   console.error("몽고디비 연결 에러", err);
+// });
+
+// module.exports = connect;
+
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const DB = mongoose.connection;;
+const DB = process.env.DB_NAME;
 
 
 const connect = () => {
-  mongoose.connect('mongodb://localhost:27017/restaurant', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+  mongoose.connect(DB, {}).catch((err) => console.log(err));
 };
-
 mongoose.connection.on("error", (err) => {
   console.error("몽고디비 연결 에러", err);
 });

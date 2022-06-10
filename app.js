4a48c7219f74c5bd2546cf562c4fcc1cd2b94316
hayeonkimm <hayeon.data@gmail.com>
2022-06-10 21:38:47 +0900

    router 추가

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const connect = require("./schemas");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+const router = express.Router();
 
 connect();
 
@@ -17,6 +18,11 @@ app.use(cors());
 app.use(express.static("static"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
+router.get('/', (req, res) => {
+  res.send('백엔드 기본 페이지 입니다..');
+});
 
 app.use("/api", [postsRouter, usersRouter, commentsRouter]);
 

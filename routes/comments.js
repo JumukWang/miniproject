8dd9a5b4fe91da76e7b74898e0c0ba928d787115
hayeonkimm <hayeon.data@gmail.com>
2022-06-10 21:38:23 +0900

    comments.js 수정

--- routes/comments.js
+++ routes/comments.js
@@ -19,11 +19,8 @@ router.post("/comments/save/:postId", authMiddleware, async (req, res) => {
     const year = today.getFullYear();
     var now = dayjs();
     var createAt = now.format();
-  
-    const createAt = createAt.slice(0, 16).split('T').join(' ');
-   
-      
-
+    createAt = createAt.slice(0, 16).split('T').join(' ');
+    
     const userId = user[0].userId;
     const userImageUrl = user[0].userImageUrl;
     const { comment } = req.body;
@@ -66,20 +63,6 @@ router.get('/comments/get/:postId', async (req, res) => {
   }
 });
 
-// router.get("/comments/get/:postId", async (req, res) => {
-
-//   const { postId } = req.params;
-//   const { commentId } = req.params;
-//   console.log(commentId)
-//   let comments = await Comment.find({ postId });
-
-
-//   res.send({ comments });
-//   console.log(comments)
-//   console.log(postId)
-
-// });
-
 
 //댓글 삭제
 
@@ -93,26 +76,3 @@ router.delete("/comments/delete/:commentId", authMiddleware, async (req, res) =>
 module.exports = router;
 
 
-
-
-
-
-// router.post("/comments/save/:postId", authMiddleware, async (req, res) => {
-//   const { user } = res.locals;
-//   console.log(user);
-//   const { comment, createAt, userImageUrl } = req.body;
-//   await Comment.create({
-//     userId: user.userId,
-//     comment,
-//     createAt,
-//     conmmentId,
-//     userImageUrl: user.userImageUrl,
-//     postId: req.params.id,
-//   });
-//   console.log(comment, createAt, userImageUrl)
-
-
-//   res.json({
-//     success: "댓글이 저장 되었습니다.",
-//   });
-// });
